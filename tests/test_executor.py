9242ba1da34dc64066b14d863f9ec6d3dbca7f11
Jakub Beránek <berykubik@gmail.com>
2019-07-30 16:51:35 +0200

    ENH: add multiprocess executor test

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -1,10 +1,12 @@
-
-
-from orco import Runtime, LocalExecutor
 import time
 
+import pytest
 
-def test_executor(env):
+from orco import LocalExecutor
+
+
+@pytest.mark.parametrize("n_processes", [None, 2])
+def test_executor(env, n_processes):
     def to_dict(lst):
         return {x["id"]: x for x in lst}
 
@@ -12,14 +14,14 @@ def test_executor(env):
     r = runtime.executor_summaries()
     assert len(r) == 0
 
-    executor = LocalExecutor(heartbeat_interval=1)
+    executor = LocalExecutor(heartbeat_interval=1, n_processes=n_processes)
     runtime.register_executor(executor)
 
-    executor2 = LocalExecutor(heartbeat_interval=1)
+    executor2 = LocalExecutor(heartbeat_interval=1, n_processes=n_processes)
     executor2._debug_do_not_start_heartbeat = True
     runtime.register_executor(executor2)
 
-    executor3 = LocalExecutor(heartbeat_interval=1)
+    executor3 = LocalExecutor(heartbeat_interval=1, n_processes=n_processes)
     runtime.register_executor(executor3)
     c = runtime.register_collection("abc")
     runtime.db.announce_entries(executor3.id, [c.ref("x")], [])
