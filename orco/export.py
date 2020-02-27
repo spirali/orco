aaa375f0fe33962149966cd88acbc2e932ce92b6
Tomas Gavenciak <gavento@gmail.com>
2020-02-27 02:10:29 +0100

    Update pandas export to have one column per argument
    * Column are named "arg.XXX" by default
    * Non-present values are filled with pandas.NA by default

--- orco/export.py
+++ orco/export.py
@@ -1,10 +1,24 @@
 import pandas as pd
 
 
-def export_builder_to_pandas(runtime, builder_name):
+def export_builder_to_pandas(runtime, builder_name, missing=pd.NA, arg_prefix="arg."):
     """
     Export builder into pandas  DataFrame
     """
-    data = [(entry.config, entry.value, entry.comp_time)
-            for entry in runtime.db.get_all_entries(builder_name)]
-    return pd.DataFrame(data, columns=["config", "value", "comp_time"])
+    cols = {"value": [], "comp_time": []}
+    for i, entry in enumerate(runtime.db.get_all_entries(builder_name)):
+        cols["value"].append(entry.value)
+        cols["comp_time"].append(entry.comp_time)
+        for k, v in entry.config.items():
+            n = arg_prefix + k
+            if n not in cols:
+                cols[n] = [missing] * i
+            c = cols[n]
+            if len(c) != i:
+                c.extend([missing] * (i - len(c)))
+            c.append(v)
+    n = len(cols["value"])
+    for c in cols.values():
+        if len(c) != n:
+            c.extend([missing] * (n - len(c)))
+    return pd.DataFrame(cols)
--- tests/test_export.py
+++ tests/test_export.py
@@ -1,5 +1,6 @@
 from orco import Builder
 from orco.export import export_builder_to_pandas
+import pandas as pd
 
 
 def test_builder_to_pandas(env):
@@ -9,7 +10,28 @@ def test_builder_to_pandas(env):
     runtime.compute_many([col1(x) for x in [1, 2, 3, 4]])
     frame = export_builder_to_pandas(runtime, col1.name)
     assert len(frame) == 4
-    assert sorted(frame["config"]) == [1, 2, 3, 4]
+    assert sorted(frame["arg.c"]) == [1, 2, 3, 4]
     assert sorted(frame["value"]) == [2, 4, 6, 8]
 
-    assert frame[frame["config"] == 1]["value"].iloc[0] == 2
+    assert frame[frame["arg.c"] == 1]["value"].iloc[0] == 2
+
+
+def test_builder_to_pandas_kwargs(env):
+    runtime = env.test_runtime()
+
+    def foo(a=1, **kwargs):
+        return a + sum(kwargs.values())
+
+    foob = runtime.register_builder(Builder(foo))
+    runtime.compute(foob(b=2))
+    runtime.compute(foob(a=3))
+    runtime.compute(foob(42, b=1, c=8))
+    frame = export_builder_to_pandas(runtime, foob.name)
+    frame.sort_values("arg.a")
+    frame.fillna(value=999, inplace=True)
+
+    assert len(frame) == 3
+    assert list(frame["arg.a"]) == [1, 3, 42]
+    assert list(frame["arg.b"]) == [2, 999, 1]
+    assert list(frame["arg.c"]) == [999, 999, 8]
+    assert list(frame["value"]) == [3, 3, 51]
