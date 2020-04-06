7141528ede3dc7e92e8b714befaf03e433acf0dd
Stanislav Bohm <spirali@kreatrix.org>
2020-04-06 23:00:12 +0200

    interactive/test.py fixed

--- tests/interactive/test.py
+++ tests/interactive/test.py
@@ -4,51 +4,58 @@ import random
 import threading
 import time
 
-from orco import Runtime, LocalExecutor
+from orco import Runtime, builder, Builder
 
+url = "sqlite:///test.db"
 if os.path.isfile("test.db"):
     os.unlink("test.db")
 
-rt = Runtime("test.db")
-
-executor = LocalExecutor(heartbeat_interval=1, n_processes=1)
-rt.register_executor(executor)
-
-executor2 = LocalExecutor(heartbeat_interval=1)
-executor2._debug_do_not_start_heartbeat = True
-rt.register_executor(executor2)
-
-executor3 = LocalExecutor(heartbeat_interval=1)
-rt.register_executor(executor3)
-executor3.stop()
-
-c_sleepers = rt.register_builder("sleepers", lambda c, d: time.sleep(c))
-c_bedrooms = rt.register_builder("bedrooms", lambda c, d: None,
-                                 lambda c: [c_sleepers.task(x) for x in c["sleepers"]])
-
-
-def failer(config, deps):
+@builder()
+def failer(config):
     raise Exception("Here!")
 
 
-c_failers = rt.register_builder("failers", failer)
+@builder()
+def sleeper(c):
+    time.sleep(c)
+
+
+@builder()
+def bedroom(sleepers):
+    [sleeper(x) for x in sleepers]
+    yield
+    return None
+
+rt = Runtime(url)
+
+
 try:
-    rt.compute(c_failers.task({"type": "fail1"}))
+    rt.compute(failer(config="fail1"))
+
 except Exception as e:
     print(e)
 print("Failer failed (and it is ok")
 
-rt.compute(c_bedrooms.task({"sleepers": [0.1]}))
-t = threading.Thread(target=(lambda: rt.compute(c_bedrooms.task({"sleepers": list(range(10))}))))
+rt.compute(bedroom(sleepers=[0.1]))
+def thread_fn():
+    rt = Runtime(url)
+    try:
+        rt.compute(bedroom(list(range(10))))
+    finally:
+        rt.stop()
+
+
+t = threading.Thread(target=thread_fn)
 t.start()
 
-time.sleep(0.5)  # To solve a problem with ProcessPool, fix waits for Python3.7
+time.sleep(0.5)
 
-c = rt.register_builder("hello")
-rt.insert(c.task("e1"), "ABC")
-rt.insert(c.task("e2"), "A" * (7 * 1024 * 1024 + 200000))
+c = rt.register_builder(Builder(None, name="hello"))
+rt.insert(c(cfg="e1"), "ABC")
+rt.insert(c(cfg="e2"), "A" * (7 * 1024 * 1024 + 200000))
 
-c = rt.register_builder("estee")
+c = Builder(None, name="estee")
+rt.register_builder(c)
 graphs = ["crossv", "fastcrossv", "gridcat"]
 models = ["simple", "maxmin"]
 scheduler = [
@@ -61,8 +68,10 @@ scheduler = [
     }
 ]
 for g, m, s in itertools.product(graphs, models, scheduler):
-    rt.insert(c.task({"graph": g, "model": m, "scheduler": s}), random.randint(1, 30000))
+    rt.insert(c(graph=g, model=m, scheduler=s), random.randint(1, 30000))
 
-c = rt.register_builder("builder with space in name")
+c = rt.register_builder(Builder(None, name="builder_long_name"))
 
+print("SERVE")
+print("=" * 80)
 rt.serve()
